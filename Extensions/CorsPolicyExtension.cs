b51707e2a4aed5ad884d96ab7db5f2ce00e56d8f
Angel Dhungana <angeldhungana32@gmail.com>
2023-02-19 19:26:23 -0700

    Registered and configured services and middlewares

---
+++ Extensions/CorsPolicyExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace JWTAuthAPI.Extensions
+{
+    public static class CorsPolicyExtension
+    {
+        private static readonly string MyAllowSpecificOrigins = "MyCorsPolicy";
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader();
+                                  });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseMyCorsPolicy(this IApplicationBuilder app)
+        {
+            app.UseCors(MyAllowSpecificOrigins);
+            return app;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,36 @@
+using FluentValidation;
+using JWTAuthAPI.Extensions;
+using JWTAuthAPI.Infrastructure.Data;
+using JWTAuthAPI.Infrastructure.Repositories;
+using JWTAuthAPI.Interfaces;
+using JWTAuthAPI.Middlewares;
+using JWTAuthAPI.Services;
+using JWTAuthAPI.Validations;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
+builder.Services.AddCorsPolicy();
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("JWTAuthAPIDb"));
+
+builder.Services.AddSwaggerCustom(builder.Configuration);
+builder.Services.AddJwtAuthentication(builder.Configuration);
+
+builder.Services.AddValidatorsFromAssemblyContaining<AuthenticateRequestValidator>();
+
+builder.Services.AddScoped<ITokenService,   JwtTokenService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddTransient<IRepositoryActivator, RepositoryActivator>();
+
 
 var app = builder.Build();
 
@@ -11,12 +38,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseRouting();
+app.UseMyCorsPolicy();
+app.UseJwtAuthorization();
 app.MapControllers();
-
 app.Run();
